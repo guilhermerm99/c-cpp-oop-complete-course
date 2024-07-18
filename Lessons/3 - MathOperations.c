85994d7bf2ee5977aff62ad0de2650eda4c0a165
Guilherme Ribeiro de Macedo <guilhermerm99@gmail.com>
2024-07-18 13:29:08 -0300

    Added math operations: addition, subtraction, multiplication, and division

---
+++ Lessons/3 - MathOperations.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Main function of the program
+void main(){
+
+    // Defining variables
+    int a = 6, b = 4;
+
+    // Sum
+    printf("\n The sum of %d e %d = %d",a , b, a + b);
+
+    // Subtraction
+    printf("\n The subtraction of %d e %d = %d",a , b, a - b);
+
+    // Division
+    printf("\n The division of %d e %d = %d",a , b, a / b);
+
+    // Multiplication
+    printf("\n The multiplication of %d e %d = %d",a , b, a * b);
+
+    // Rest of the Division
+    printf("\n The remainder of the division between %d e %d = %d",a , b, a % b);
+
+    // Absolute Value
+    printf("\n The absolute value of -3 = %d", abs(-3));
+
+    // Pause the program after running
+    system("pause");
+
+}
